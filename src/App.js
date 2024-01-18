0ce653498ce6f1bd158549a74c93dc1c74de1025
aniketpanche <aniketpanche018@gmail.com>
2024-01-18 21:25:16 +0530

    thirdcommit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
      items-center bg-gray-200">
       <div className=" text-center">
         <h1 className="text-4xl font-bold">
-          Our Testimonials
+          Our Testimonials are here
         </h1>
         <div className=" bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
         <Testimonials reviews={reviews}/>
